60311b9bc2a501183f1538b61f4d9ae31fb3577a
flmgch <nvelasco@frba.utn.edu.ar>
2025-09-10 16:20:03 -0300

    app levanta

--- routes/pedidoRoutes.js
+++ routes/pedidoRoutes.js
@@ -7,14 +7,17 @@ const pathPedido = "/pedido"
 export default function pedidosRoutes(getController) {
     const router = express.Router()
 
-    router.patch(pathPedido + "/:id", (res,req) => {
+    router.patch(pathPedido + "/:id", (req,res) => {
         getController(PedidoController).cancelar(req,res)
     })
 
-
-
+    router.get(pathPedido + "/:id", (req, res) => {
+        getController(PedidoController).consultar(req, res)
+    })
 
     router.post(pathPedido, (req, res) => {
         getController(PedidoController).crear(req, res)
     })
+
+    return router
 }
