94aad1cbcf81bfa8aa3153f9c18bca4e39df77b9
agustinaduric <130220670+agustinaduric@users.noreply.github.com>
2025-10-12 20:29:56 -0300

    swagger funcionando, falta actualizarlo

--- routes/swaggerRoutes.js
+++ routes/swaggerRoutes.js
@@ -3,14 +3,29 @@ import swaggerUi from "swagger-ui-express"
 import YAML from "yamljs"
 
 const router = express.Router()
-const usuarios = YAML.load(`${process.cwd()}/docs/usuarios.yaml`)
-const productos = YAML.load(`${process.cwd()}/docs/productos.yaml`)
+
 const notificaciones = YAML.load(`${process.cwd()}/docs/notificaciones.yaml`)
 const pedidos = YAML.load(`${process.cwd()}/docs/pedidos.yaml`)
+const productos = YAML.load(`${process.cwd()}/docs/productos.yaml`)
+const usuarios = YAML.load(`${process.cwd()}/docs/usuarios.yaml`)
 
-router.use("/usuarios", swaggerUi.serve, swaggerUi.setup(usuarios))
-router.use("/productos", swaggerUi.serve, swaggerUi.setup(productos))
-router.use("/notificaciones", swaggerUi.serve, swaggerUi.setup(notificaciones))
-router.use("/pedidos", swaggerUi.serve, swaggerUi.setup(pedidos))
+router.get("/notificaciones.yaml", (req, res) => res.json(notificaciones))
+router.get("/pedidos.yaml", (req, res) => res.json(pedidos))
+router.get("/productos.yaml", (req, res) => res.json(productos))
+router.get("/usuarios.yaml", (req, res) => res.json(usuarios))
+
+const options = {
+  explorer: true,
+  swaggerOptions: {
+    urls: [
+        { url: "/docs/notificaciones.yaml", name: "Notificaciones" },
+        { url: "/docs/pedidos.yaml", name: "Pedidos" },
+        { url: "/docs/productos.yaml", name: "Productos" },
+        { url: "/docs/usuarios.yaml", name: "Usuarios" },
+    ]
+  }
+}
+
+router.use("/", swaggerUi.serve, swaggerUi.setup(null, options))
 
 export default router
