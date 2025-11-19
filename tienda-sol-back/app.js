ab9be4c7f28ce11c546e8c01d0a88ccc3ab60861
MilagrosLu <msalafia@frba.utn.edu.ar>
2025-11-19 14:09:52 -0300

    netlify

--- tienda-sol-back/app.js
+++ tienda-sol-back/app.js
@@ -9,6 +9,13 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization', 'X-User']
 }))
 
+app.use(cors({
+  origin: 'https://tiendasol10.netlify.app',
+  credentials: true,
+  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-User']
+}))
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
